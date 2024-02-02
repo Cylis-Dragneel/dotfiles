32db6d868d97974b265d413a85bccb9d7610d767
BlackPhoenix1747 <craftingcaptain456@gmail.com>
2024-02-02 21:21:50 +0500

    nvim config modified

--- .config/nvim/lua/plugins/configs/lazy_nvim.lua
+++ .config/nvim/lua/plugins/configs/lazy_nvim.lua
@@ -1,1 +1,47 @@
-../../../../../dotfiles/.config/nvim/lua/plugins/configs/lazy_nvim.lua
+return {
+  defaults = { lazy = true },
+  install = { colorscheme = { "nvchad" } },
+
+  ui = {
+    icons = {
+      ft = "",
+      lazy = "󰂠 ",
+      loaded = "",
+      not_loaded = "",
+    },
+  },
+
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "2html_plugin",
+        "tohtml",
+        "getscript",
+        "getscriptPlugin",
+        "gzip",
+        "logipat",
+        "netrw",
+        "netrwPlugin",
+        "netrwSettings",
+        "netrwFileHandlers",
+        "matchit",
+        "tar",
+        "tarPlugin",
+        "rrhelper",
+        "spellfile_plugin",
+        "vimball",
+        "vimballPlugin",
+        "zip",
+        "zipPlugin",
+        "tutor",
+        "rplugin",
+        "syntax",
+        "synmenu",
+        "optwin",
+        "compiler",
+        "bugreport",
+        "ftplugin",
+      },
+    },
+  },
+}
--- .config/nvim/lua/plugins/configs/treesitter.lua
+++ .config/nvim/lua/plugins/configs/treesitter.lua
@@ -1,1 +1,12 @@
-../../../../../dotfiles/.config/nvim/lua/plugins/configs/treesitter.lua
+local options = {
+  ensure_installed = { "lua" },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
