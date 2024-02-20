058bf4f4bc5efe42b25a7da421f3d7fa18598df0
BlackPhoenix1747 <craftingcaptain456@gmail.com>
2024-02-20 22:03:01 +0500

    Removed commits of other repos in the Obsidian Vault backup script

--- bin/UpdateObsidian.sh
+++ bin/UpdateObsidian.sh
@@ -3,24 +3,9 @@
 #directory = $(cd ~/ && fzf)
 
 #Getting to desired git repository
-cd ~/dotfiles/
-
-#Git update
-git add *
-git add .*
-git commit -m "Changes Made"
-git push origin main
-
-#Getting to desired git repository
 cd ~/Documents/Obsidian/
 
-git add *
-git add .*
+git add .
 git commit -m "Changes Made"
 git push origin main
 
-cd ~/Portfolio/
-
-git add *
-git commit -m "Changes Made"
-git push origin main
