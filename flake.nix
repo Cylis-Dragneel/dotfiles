69294ce028ed83bdcb25b9059b7fe31ddb91a6f7
Taha Ahmad <craftingcaptain456@gmail.com>
2024-10-19 15:32:10 +0500

    Updated flake, added new inputs, added a new host (Laptop)

--- flake.nix
+++ flake.nix
@@ -1,22 +1,29 @@
 {
-  description = "ZaneyOS";
+  description = "CylisOS";
 
   inputs = {
     jerry.url = "github:justchokingaround/jerry";
+    nyaa = {
+      url = "github:Beastwick18/nyaa";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     stylix.url = "github:danth/stylix";
     zig.url = "github:mitchellh/zig-overlay";
+    ghostty.inputs.nixpkgs-stable.follows = "nixpkgs";
+    ghostty.inputs.nixpkgs-unstable.follows = "nixpkgs";
     ghostty.url = "git+ssh://git@github.com/ghostty-org/ghostty";
     spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    pollymc = {
-      url = "github:fn2006/PollyMC";
-    };
+    zen.url = "github:MarceColl/zen-browser-flake";
+    # pollymc = {
+    #   url = "github:fn2006/PollyMC";
+    # };
     fine-cmdline = {
       url = "github:VonHeikemen/fine-cmdline.nvim";
       flake = false;
@@ -30,10 +37,18 @@
   };
 
   outputs =
-    { nixpkgs, home-manager, nixpkgs-stable, zig, ghostty, spicetify-nix, pollymc, ... }@inputs:
+    {
+      nixpkgs,
+      home-manager,
+      nixpkgs-stable,
+      ghostty,
+      zig,
+      spicetify-nix,
+      ...
+    }@inputs:
     let
       system = "x86_64-linux";
-      host = "dragneel";
+      host = "based";
       username = "cylis";
     in
     {
@@ -49,30 +64,34 @@
             ./hosts/${host}/config.nix
             inputs.stylix.nixosModules.stylix
             home-manager.nixosModules.home-manager
-            ({pkgs, ...}: {
-              nixpkgs.overlays = [pollymc.overlays.default];
-              environment.systemPackages = [
-                ghostty.packages.x86_64-linux.default
-                pkgs.pollymc
-              ];
-              home-manager.extraSpecialArgs = {
-                inherit username;
-                inherit inputs;
-                inherit host;
-                inherit spicetify-nix;
-                pkgs-stable = import nixpkgs-stable {
-                  inherit system;
-                  inherit inputs;
+            (
+              { pkgs, ... }:
+              {
+                #nixpkgs.overlays = [pollymc.overlays.default];
+                environment.systemPackages = [
+                  ghostty.packages.x86_64-linux.default
+                  inputs.zen.packages.x86_64-linux.default
+                  #pkgs.pollymc
+                ];
+                home-manager.extraSpecialArgs = {
                   inherit username;
+                  inherit inputs;
                   inherit host;
-                  config.allowUnfree = true;
+                  inherit spicetify-nix;
+                  pkgs-stable = import nixpkgs-stable {
+                    inherit system;
+                    inherit inputs;
+                    inherit username;
+                    inherit host;
+                    config.allowUnfree = true;
+                  };
                 };
-              };
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.backupFileExtension = "backup";
-              home-manager.users.${username} = import ./hosts/${host}/home.nix;
-            })
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.backupFileExtension = "bak";
+                home-manager.users.${username} = import ./hosts/${host}/home.nix;
+              }
+            )
           ];
         };
       };
